97be12164363960f93697b876ee0732af53f8759
Michael Zalla <michael.zalla@gmail.com>
2014-12-14 04:59:34 -0500

    Added support for setting the Logger filter to include all environments; added 'Fizz.throws' method as shorthand reference to 'Logger.prototype.error';

--- js/lib/Fizz.Logger.js
+++ js/lib/Fizz.Logger.js
@@ -7,51 +7,56 @@ this.Fizz = this.Fizz || { };
 
 	var Logger = function() {
 		if(Fizz.logger) {
-			Fizz.logger.error("Error: The Logger class cannot be instantiated!");
+			Fizz.throws("Error: The Logger class cannot be instantiated!");
 		}
 	};
 
 	Logger.prototype.filter = function(env) {
-		_currentEnvFilter = env.toString().toLowerCase();
+		_currentEnvFilter = ("all" === env) ?
+			Fizz.getEnv() : env.toString().toLowerCase();
 		return this;
 	};
 
 	Logger.prototype.log = function() {
 		if(_currentEnvFilter !== Fizz.getEnv()) return;
-		window.console.log("[" + Fizz.getEnv().toUpperCase() +
-			"] " + String.format.apply(this, arguments));
+		window.console.log("Fizz [" + Fizz.getEnv().toUpperCase() + "]\u0020\u0020" +
+			String.format.apply(this, arguments));
 		return this;
 	};
 
 	Logger.prototype.debug = function() {
 		if(_currentEnvFilter !== Fizz.getEnv()) return;
-		window.console.debug("[" + Fizz.getEnv().toUpperCase() +
-			"] " + String.format.apply(this, arguments));
+		window.console.debug("Fizz [" + Fizz.getEnv().toUpperCase() + "]\u0020\u0020" +
+			String.format.apply(this, arguments));
 		return this;
 	};
 
 	Logger.prototype.info = function() {
 		if(_currentEnvFilter !== Fizz.getEnv()) return;
-		window.console.info("[" + Fizz.getEnv().toUpperCase() +
-			"] " + String.format.apply(this, arguments));
+		window.console.info("Fizz [" + Fizz.getEnv().toUpperCase() + "]\u0020\u0020" +
+			String.format.apply(this, arguments));
 		return this;
 	};
 
 	Logger.prototype.warn = function() {
 		if(_currentEnvFilter !== Fizz.getEnv()) return;
-		window.console.warn("[" + Fizz.getEnv().toUpperCase() +
-			"] " + String.format.apply(this, arguments));
+		window.console.warn("Fizz [" + Fizz.getEnv().toUpperCase() + "]\u0020\u0020" +
+			String.format.apply(this, arguments));
 		return this;
 	};
 
 	Logger.prototype.error = function() {
 		if(_currentEnvFilter !== Fizz.getEnv()) return;
-		window.console.error("[" + Fizz.getEnv().toUpperCase() +
-			"] " + String.format.apply(this, arguments));
-		return this;
+		throw new Error("Fizz [" + Fizz.getEnv().toUpperCase() + "]\u0020\u0020" +
+			String.format.apply(this, arguments));
 	};
 
 	// Singleton export
 	Fizz.logger = new Logger();
+
+	// Shorthand reference
+	Fizz.throws = function(err) {
+		this.filter("all").error(err);
+	}.bind(Fizz.logger);
 
 }());
